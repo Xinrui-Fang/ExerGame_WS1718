89cf4a30e49d7807b5514ca9fdad719645c2518c
Jan Friebertshäuser <freebejan@googlemail.com>
2017-11-21 14:40:11 +0100

    Interfaces for GameManager and Menu as well as Settings class.

---
+++ Assets/WorldControl/GameSettings.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings
+{
+    public enum SpikyNess
+    {
+        Normal,
+        Rigid,
+        Very_rigid,
+        Extreme
+    }
+
+    public enum TerrainType
+    {
+        Mountains,
+        Valley,
+        Mixed
+    }
+
+    public enum Moisture
+    {
+        Dry,
+        Wet,
+        Mixed
+    }
+    
+    // How hight the heighest mountain can be in unity units. Values should be between 150 and 350
+    public int MaxMountainHeight { get; set; }
+
+    // How spikey the mountains should be
+    public SpikyNess MountainSpikyNess { get; set; }
+
+    // Is the world made up more of mountains or more of valleys?
+    public TerrainType WorldTerrainType { get; set; }
+
+    // Is it a dry region like a desert or a "wet" like a rainforest?
+    public Moisture WorldMoisture { get; set; }
+
+    //  The Seed for the world.
+    public long WorldSeed { get; set; }
+
+    // How detailed should the terrain be? Values should be between 2 and 8
+    public int LevelOfTerrainDetail { get; set; }
+}
---
+++ Assets/WorldControl/WorldControlInterfaces.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IGameManager
+{
+    // Sets reference to the main menu
+    void SetMenu(IMenu menu);
+
+    // Called by Menu to start the game.
+    void StartGame(GameSettings settings);
+}
+
+public interface IMenu
+{
+    // This function will be called by the GameManager to return to Main Menu
+    void ShowMenu();
+
+    // This funciton will be called by GameManager to show the ScoreBoard;
+    void ShowScore();
+
+    // will be called by GameManger to add new Score. Player should be prompted to input his name.
+    void AddScore(float Score);
+}
