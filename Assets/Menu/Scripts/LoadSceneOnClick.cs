c5a7ad86979a4b3fa2f5e443cdffd4a387dd71e0
Nenya <laureline.leduigou@orange.fr>
2017-11-25 13:34:55 +0100

    Menu add

---
+++ Assets/Menu/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSceneOnClick : MonoBehaviour {
+	
+
+	public void LoadByIndex(int sceneIndex)
+	{
+		
+		Dropdown[] Parameters = GetComponents<Dropdown>();
+		foreach (Dropdown dropdown in Parameters) {
+			Dropdown.OptionData options = dropdown.options [dropdown.value];
+			PlayerPrefs.SetString (dropdown.ToString (), options.text);
+		}
+		SceneManager.LoadScene (sceneIndex);
+
+	}
+}
