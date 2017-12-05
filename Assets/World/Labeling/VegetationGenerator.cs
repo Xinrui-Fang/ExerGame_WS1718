bb7a490b9af4e4b4204219164ee5b52d967bcc5c
Yannick Pflanzer <yp1995@outlook.com>
2017-12-05 14:06:55 +0100

    Implemented random tree placement

--- Assets/World/Labeling/VegetationGenerator.cs
+++ Assets/World/Labeling/VegetationGenerator.cs
@@ -13,7 +13,7 @@ public class VegetationGenerator
 
     public TerrainData PaintGras(INoise2DProvider noise, TerrainData terrainData)
     {
-        for (int l = 0; l < NumberOfLayers; l++)
+	for (int l = 0; l < NumberOfLayers; l++)
         {
 
             int[,] detailMap = new int[terrainData.detailWidth, terrainData.detailHeight];
@@ -28,6 +28,38 @@ public class VegetationGenerator
             }
             terrainData.SetDetailLayer(0, 0, l, detailMap);
         }
-        return terrainData;
+        
+    
+	int Seed = 42; // Randomly chosen by chance!
+	System.Random prng = new System.Random((int) Seed);
+
+	TreePrototype proto = new TreePrototype();
+	proto.prefab = GameObject.Find("FirstPersonTree");
+	
+	TreePrototype[] protos = terrainData.treePrototypes;
+	System.Array.Resize<TreePrototype>(ref protos, terrainData.treePrototypes.Length + 1);
+	protos[terrainData.treePrototypes.Length] = proto;
+	
+	terrainData.treePrototypes = protos;
+	
+	TreeInstance[] trees = new TreeInstance[512];
+	for(int i = 0; i < trees.Length; i++)
+	{
+		trees[i] = new TreeInstance();
+		trees[i].prototypeIndex = 0;
+		
+		int x = prng.Next(0, terrainData.heightmapWidth);
+		int y = prng.Next(0, terrainData.heightmapHeight);
+		trees[i].position = new Vector3((float) x / terrainData.heightmapWidth, 
+					terrainData.GetHeight(x, y) / terrainData.size.y,
+					(float) y / terrainData.heightmapHeight);
+		trees[i].heightScale = (float) prng.Next(256, 512) / 512.0f;
+		trees[i].widthScale = trees[i].heightScale;
+		
+		trees[i].rotation = (float) prng.Next(0, 360) * Mathf.Deg2Rad;
+	}	
+	
+	terrainData.treeInstances = trees;
+	return terrainData;
     }
 }
