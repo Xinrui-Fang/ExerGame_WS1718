14fb8d632f57b7ed8e0762b2aa665477251da933
Jan Friebertshäuser <freebejan@googlemail.com>
2017-12-10 00:48:02 +0100

    FineTuning Terrain

--- Assets/World/Labeling/TerrainLabeler.cs
+++ Assets/World/Labeling/TerrainLabeler.cs
@@ -70,7 +70,7 @@ public static class TerrainLabeler
                     splatWeights[0] = moisture*Mathf.InverseLerp(.6f, 1f, normal.y) * .2f * Mathf.Pow(Mathf.Clamp(0, .2f, .25f - Mathf.Abs(.25f - height)), 2f);
                     splatWeights[1] = Mathf.InverseLerp(.6f, 1f, normal.y) * Mathf.Pow(Mathf.InverseLerp(.6f, 1f, height), 2f) * moisture;
                     splatWeights[2] = 4 * (1f -Mathf.InverseLerp(0f, .6f, normal.y)) * (1f - moisture);
-                    splatWeights[3] = steepness * Mathf.Pow(Mathf.InverseLerp(.8f, 1f, 1f - height), 2f) * (1f - moisture);
+                    splatWeights[3] = (1f - Mathf.InverseLerp(0f, .5f, normal.y)) * Mathf.Pow(Mathf.InverseLerp(.8f, 1f, 1f - height), 2f) * (1f - moisture);
                     //splatWeights[4] = splatWeights[0] * .7f + splatWeights[3] * .5f * (1f-moisture);
                     splatWeights[5] = 3 * Mathf.Abs(normal.z) * Mathf.Abs(normal.x) * Mathf.InverseLerp(.2f, 1f, height) * (1f - moisture);
                 }
