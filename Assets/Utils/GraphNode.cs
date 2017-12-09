2a030359d77c3b2537ff3b2adc3c857dd5b406d1
Jan Friebertshäuser <freebejan@googlemail.com>
2017-12-09 11:32:51 +0100

    Cluster Interface idea.

---
+++ Assets/Utils/GraphNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Utils
+{
+    public struct GraphNode
+    {
+        public int Value;
+        public Vector2Int location;
+        public List<GraphNode> Neightbors;
+    }
+}
