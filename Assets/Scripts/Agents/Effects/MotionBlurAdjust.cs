2742de0201945cb108277a9b0f53e2bec6120b4a
Jan Friebertshäuser <freebejan@gmail.com>
2018-03-06 06:06:04 +0100

    Dynamically Adjust Motion Blur to speed

---
+++ Assets/Scripts/Agents/Effects/MotionBlurAdjust.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+/// <summary>
+/// Adjusts motion blur based on player speed;
+/// </summary>
+public class MotionBlurAdjust : MonoBehaviour
+{
+
+	public PostProcessingProfile profile;
+	private BikeState state;
+	public float defaultAngle = 20f;
+	public int defaultSampleCount = 20;
+	public float speedModifier = 1f;
+	private int counter = 0;
+	public int refreshInterval = 3;
+
+	// Use this for initialization
+	void Start()
+	{
+		state = GetComponent<BikeState>();
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		counter++;
+		if (counter >= refreshInterval)
+		{
+			counter = 0;
+			var settings = new MotionBlurModel.Settings
+			{
+				shutterAngle = defaultAngle + speedModifier * state.Speed,
+				sampleCount = defaultSampleCount,
+				frameBlending = 0
+			};
+			profile.motionBlur.settings = settings;
+		}
+	}
+}
