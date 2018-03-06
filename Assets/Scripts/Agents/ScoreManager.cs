77eff6da6760128a4d692617fac62a93d98e1c48
Yannick Pflanzer <yp1995@outlook.com>
2018-03-06 10:52:58 +0100

    Implemented a basic score manager with HUD.

---
+++ Assets/Scripts/Agents/ScoreManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour 
+{
+	public int Score = 0;
+	public float Multiplier = 10.0f;
+	
+	private Vector3 LastPosition = new Vector3(); 
+	
+	void Start()
+	{
+		LastPosition = gameObject.transform.position;
+	}
+	
+	void Update()
+	{
+		var diff = LastPosition - gameObject.transform.position;
+		LastPosition = gameObject.transform.position;
+		
+		Score += (int) (diff.magnitude * Multiplier);
+	}
+	
+	void OnGUI()
+	{
+		 GUI.Label(new Rect(25, 25, 100, 30), "Score: " + Score);
+	}
+}
+ 
